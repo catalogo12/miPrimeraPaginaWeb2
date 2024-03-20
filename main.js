4d7ce85cfff633eace5059aed80f822a50dbb754
catalogo12 <gutierrez54020@gmail.com>
2024-03-19 18:44:31 -0600

    casi terminado

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
-const adquirirDatos=()=>{
-    let listaFormlulario=[];
-    let valores=document.getElementsByTagName("input");
-    console.log(valores);
-    for(let i=0;i<valores.length;i++){
-        let valoresNuevos=listaFormlulario.push(valores[i].value);
-        console.log(valoresNuevos);
-        return(listaFormlulario);
-    }
+let productos=document.getElementsByTagName("input");
+let boton=document.querySelector("button");
+console.log(productos);
+// function funcion1(){
+//     let listaProduct1=[];
+//     for(let i=0;i<productos.length;i++){
+//         let valor=productos[i].value;
 
-}
-let btn=document.getElementsByTagName("button");
-console.log(btn);
-let a1= document.getElementById("input1");
-console.log(a1);
-btn.addEventListenet=("click",adquirirDatos());
-let resultado=adquirirDatos();
-console.log(resultado);
+//         listaProduct1.push(valor);
+            
+        
+//     }
+//     console.log(listaProduct1);
+//     return listaProduct1;
+//}
+boton.addEventListener("click",function(event){
+    event.preventDefault();
+    let listaProduct1=[];
+    for(let i=0;i<productos.length;i++){
+        let valor=productos[i].value;
+        if(i%2==1){
+
+        
+
+        listaProduct1.push(valor);
+        }
+        
+    }
+    console.log(listaProduct1);
+    let sum=0;
+    for(let i=0;i<listaProduct1.length;i++){
+        sum=sum+parseFloat(listaProduct1[i]);
+    }
+    console.log(sum);
+    let nuevoEscrito=document.createElement("h2");
+    let nuevoElemento=document.createElement("h1");
+    nuevoEscrito.textContent="suma total";
+    nuevoElemento.textContent=sum;
+    document.body.appendChild(nuevoEscrito);
+    document.body.appendChild(nuevoElemento);
+    return sum;
+});
